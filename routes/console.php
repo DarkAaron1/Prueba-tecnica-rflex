16a96ad2cebef8f8ec4fddb9e6bd9cdff2e22fa1
DarkAaron1 <felipe.burgos1@virginiogomez.cl>
2026-02-25 19:38:18 -0300

    job en ruta/console

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\ProcessAttendanceJob;
+
+Schedule::job(ProcessAttendanceJob::class)->everyFiveMinutes(); /* Hace que el Job Se ejecute en el rango de tiempo */
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
